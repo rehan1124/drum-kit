efd0f9db9b33b95f5c07b51ad43b2ab50bc47018
srehan1 <syed.rehan@shoppertrak.com>
2022-09-12 12:11:03 +0530

    Anonymous function

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,14 @@ function playSound() {
   alert("I am clicked !!!");
 }
 
+/*
+//OR
+// Equivalent anonymous function will be
+function () {
+    alert("I am clicked !!!");
+  }
+*/
+
 // Event listeners
 for (var i = 0; i < drumBtn.length; i += 1) {
   drumBtn[i].addEventListener("click", playSound);
