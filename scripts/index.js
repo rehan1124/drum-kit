4946020041f048af9c63088ebf1391709e858d0d
srehan1 <syed.rehan@shoppertrak.com>
2022-09-12 11:42:31 +0530

    1- Changed footer-tag position, 2- Checking event listeners

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Locate drum buttons
+var drumBtn = document.querySelectorAll(".drum");
+
+// Define function to be executed
+function playSound() {
+  alert("I am clicked !!!");
+}
+
+// Event listeners
+for (var i = 0; i < drumBtn.length; i += 1) {
+  drumBtn[i].addEventListener("click", playSound);
+}
