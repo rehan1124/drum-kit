3a1e9be428d71475db3b5be4bcbfdf884a7b18ce
srehan1 <syed.rehan@shoppertrak.com>
2022-09-13 18:48:38 +0530

    1- Added images & sounds to buttons, 2- Change color on button click

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,27 @@ var drumBtn = document.querySelectorAll(".drum");
 
 // Define function to be executed
 function playSound() {
-  alert("I am clicked !!!");
+  var sound = "";
+
+  // console.log(this.textContent);
+  if (this.textContent === "w") {
+    sound = "../sounds/crash.mp3";
+  } else if (this.textContent === "a") {
+    sound = "../sounds/kick-bass.mp3";
+  } else if (this.textContent === "s") {
+    sound = "../sounds/snare.mp3";
+  } else if (this.textContent === "d") {
+    sound = "../sounds/tom-1.mp3";
+  } else if (this.textContent === "j") {
+    sound = "../sounds/tom-2.mp3";
+  } else if (this.textContent === "k") {
+    sound = "../sounds/tom-3.mp3";
+  } else if (this.textContent === "l") {
+    sound = "../sounds/tom-4.mp3";
+  }
+  var audio = new Audio(sound);
+  audio.play();
+  this.style.color = "white";
 }
 
 /*
