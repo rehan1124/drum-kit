d3c139ae0662ffae7467f156fa3053966ad1b352
srehan1 <syed.rehan@shoppertrak.com>
2022-09-13 21:16:08 +0530

    1- Added sounds for all button press, 2- Replace if/else with switch

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,34 @@ function playSound() {
   var sound = "";
 
   // console.log(this.textContent);
+  // Instead of if - else if - else, used switch
+  switch (this.textContent) {
+    case "w":
+      sound = "../sounds/crash.mp3";
+      break;
+    case "a":
+      sound = "../sounds/kick-bass.mp3";
+      break;
+    case "s":
+      sound = "../sounds/snare.mp3";
+      break;
+    case "d":
+      sound = "../sounds/tom-1.mp3";
+      break;
+    case "j":
+      sound = "../sounds/tom-2.mp3";
+      break;
+    case "k":
+      sound = "../sounds/tom-3.mp3";
+      break;
+    case "l":
+      sound = "../sounds/tom-4.mp3";
+      break;
+    default:
+      console.log("Press buton shown on screen !!!");
+  }
+
+  /*
   if (this.textContent === "w") {
     sound = "../sounds/crash.mp3";
   } else if (this.textContent === "a") {
@@ -23,6 +51,8 @@ function playSound() {
   } else if (this.textContent === "l") {
     sound = "../sounds/tom-4.mp3";
   }
+  */
+
   var audio = new Audio(sound);
   audio.play();
   this.style.color = "white";
