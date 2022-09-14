fb2c11dd6af7b18361254243990345e9c10d9a2d
srehan1 <syed.rehan@shoppertrak.com>
2022-09-14 11:27:52 +0530

    Added event listeners for keydown

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,10 @@
 // Locate drum buttons
 var drumBtn = document.querySelectorAll(".drum");
 
-// Define function to be executed
-function playSound() {
+function playSound(event) {
   var sound = "";
-
-  // console.log(this.textContent);
-  // Instead of if - else if - else, used switch
-  switch (this.textContent) {
+  // console.log(event.key);
+  switch (event) {
     case "w":
       sound = "../sounds/crash.mp3";
       break;
@@ -35,38 +32,19 @@ function playSound() {
       console.log("Press buton shown on screen !!!");
   }
 
-  /*
-  if (this.textContent === "w") {
-    sound = "../sounds/crash.mp3";
-  } else if (this.textContent === "a") {
-    sound = "../sounds/kick-bass.mp3";
-  } else if (this.textContent === "s") {
-    sound = "../sounds/snare.mp3";
-  } else if (this.textContent === "d") {
-    sound = "../sounds/tom-1.mp3";
-  } else if (this.textContent === "j") {
-    sound = "../sounds/tom-2.mp3";
-  } else if (this.textContent === "k") {
-    sound = "../sounds/tom-3.mp3";
-  } else if (this.textContent === "l") {
-    sound = "../sounds/tom-4.mp3";
-  }
-  */
-
   var audio = new Audio(sound);
   audio.play();
-  this.style.color = "white";
 }
 
-/*
-//OR
-// Equivalent anonymous function will be
-function () {
-    alert("I am clicked !!!");
-  }
-*/
-
-// Event listeners
+// Event listener for button click
 for (var i = 0; i < drumBtn.length; i += 1) {
-  drumBtn[i].addEventListener("click", playSound);
+  drumBtn[i].addEventListener("click", function () {
+    var button = this.textContent;
+    playSound(button);
+  });
 }
+
+// Event listener for keyboard key press
+document.addEventListener("keydown", function (event) {
+  playSound(event.key);
+});
